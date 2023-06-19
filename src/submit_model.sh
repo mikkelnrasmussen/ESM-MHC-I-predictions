c3bd400a7196aaf26bdebca0489fe77b0ba5c409
mikkelnrasmussen <s193518@student.dtu.dk>
2023-06-19 14:34:30 +0200

    analys: source virtual enviroment

--- src/submit_model.sh
+++ src/submit_model.sh
@@ -3,13 +3,13 @@
 ### –- specify queue --
 #BSUB -q gpuv100
 ### -- set the job Name --
-#BSUB -J ESM-MHC-NN
+#BSUB -J blossum-sigmoid
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 1
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 24:00
+#BSUB -W 10:00
 # request 32GB of system-memory
 #BSUB -R "rusage[mem=32GB]"
 ### -- set the email address --
@@ -29,4 +29,5 @@
 # Create time dir
 timeDir=time
 mkdir -p $timeDir
+source ../esm-venv/bin/activate
 /usr/bin/time -v -o $timeDir/5_fold_CV.time bash 5_fold_CV.sh
